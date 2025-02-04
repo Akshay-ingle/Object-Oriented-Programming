fc84ac83f7408c878f76cd1d3fb2e9fddaefed5f
Akshay-ingle <akshy.nilesh.ingle@gmail.com>
2025-02-04 19:40:33 +0530

    vehical

---
+++ classvehical.py
@@ -0,0 +1,7 @@
+class vehical:
+    def __init__(self,milage,maxSpeed):
+        self.milage=milage
+        self.maxSpeed=maxSpeed
+ob= vehical(20,50)
+print('the milage of the bus is ',ob.milage)
+print('the max speed of the bus is ',ob.maxSpeed)
